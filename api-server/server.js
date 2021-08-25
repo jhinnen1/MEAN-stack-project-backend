288e6caf96ed5182c4b9eb8e2d4c57bef5c947e6
Jerimiah W. Hinnen <70825145+jhinnen1@users.noreply.github.com>
2021-08-25 16:28:31 -0500

    Update server.js

--- api-server/server.js
+++ api-server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 app.use(cors());
 
-//CORS configuration
+// Configuration
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'DELETE, POST, PUT');
